baa7830cb3a655bebf976739385d581ea11c3793
Jay <creepybrow@juno.com>
2024-12-30 10:57:40 -0800

    Test commit

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,12 +1,39 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import SubHeader from "../images/subheader.jpg";
 import ExploreItems from "../components/explore/ExploreItems";
 
 const Explore = () => {
+  const [data, setData] = useState([]);//State to store the API response
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     window.scrollTo(0, 0);
     //Does this work
   }, []);
+
+  useEffect(() =>{
+    window.scrollTo(0,0);
+
+    //Fetch data from the API
+
+    const fetchData = async () => {
+      try{
+        const response = await fetch('https://us-central1-nft-cloud-functions.cloudfunctions.net/hotCollections');
+        const result = await response.json();
+        setData(result);//Set the data to state
+      }catch(error){
+        console.error("Error fetching data:", error);
+      }finally{
+        setLoading(false);//set Loading to false once data is fetched
+      }
+    };
+
+    fetchData();//Call the fetch function
+  }, []);// Empty dependency array means this effect runs once when the component mounts
+
+  //If data is still loading, show a loading message
+  if(loading){
+    return <div>Loading...</div>;
+  }
 
   return (
     <div id="wrapper">
